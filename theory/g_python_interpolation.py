7b9cac2d8075dda20f7b42d8cf63204e145bbed5
Jonny Daenen <jonnydaenen@gmail.com>
2023-09-13 20:46:04 +0200

    Add extra interpolation example

--- theory/g_python_interpolation.py
+++ theory/g_python_interpolation.py
@@ -1,8 +1,15 @@
 
 
+
 weight = 7000
+print(f"Today's weight is {weight} g")
+
 output1 = f"Today's weight is " + str(weight / 1000) +" kg"
 output2 = f"Today's weight is {weight / 1000} kg"
 print(output1)
 print(output2)
-print(f"Today's weight is {weight} g")
+
+def calculation():
+    return weight / 1000
+
+print(f"Today's weight is {calculation()} kg")
