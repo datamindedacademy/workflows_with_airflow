0c7b55c7ee02a8324dc3df7384836115c4cbb5ba
Jonny Daenen <jonnydaenen@gmail.com>
2023-09-13 20:45:07 +0200

    Add string interpolation example

---
+++ theory/g_python_interpolation.py
@@ -0,0 +1,8 @@
+
+
+weight = 7000
+output1 = f"Today's weight is " + str(weight / 1000) +" kg"
+output2 = f"Today's weight is {weight / 1000} kg"
+print(output1)
+print(output2)
+print(f"Today's weight is {weight} g")
