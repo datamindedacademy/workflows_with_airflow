376be58b3181787fdef84e994eea18b8d2c13e1c
Jos Teunissen <jlteunissen@gmail.com>
2026-02-02 14:48:34 +0000

    make exercise 8 more interesting by adding retries

--- workspace/exercises/8_ignoring_failure.py
+++ workspace/exercises/8_ignoring_failure.py
@@ -14,18 +14,38 @@ import random
 
 from airflow import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
-from airflow.providers.standard.operators.python import BranchPythonOperator
+from airflow.providers.standard.operators.python import BranchPythonOperator, PythonOperator
+
+default_args = {
+    "owner": "Airflow",
+    "retries": 3,
+    "retry_delay": dt.timedelta(minutes=1),
+}
 
 dag = DAG(
     dag_id="8_ignoring_failure",
     description="Many tasks in parallel",
-    default_args={"owner": "Airflow"},
+    default_args=default_args,
     schedule="@daily",
     start_date=dt.datetime(2026, 1, 1),
     end_date=dt.datetime(2026, 3, 1),
+    max_active_runs=3,
+    catchup=True
 )
 
-dummies = [EmptyOperator(task_id=f"task{n}", dag=dag) for n in range(7)]
+
+def fail_on_odd_try(ti):
+    print("ti:", ti)
+    try_number = ti.try_number
+    if try_number % 2 == 1:
+        raise Exception(f"Failing on odd try number: {try_number}")
+
+
+dummies = [
+    PythonOperator(task_id="task2", dag=dag, python_callable=fail_on_odd_try) if n == 2
+    else EmptyOperator(task_id=f"task{n}", dag=dag)
+    for n in range(7)
+]
 
 def split():
     index = 1 + int(random.random() > .5)
