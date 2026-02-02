0e3de04cc34b3940f0de3ea9612b79bf3f0eaca7
Jos Teunissen <jlteunissen@gmail.com>
2026-02-02 12:19:56 +0000

    make exercise 6 sensor deferrable

--- solutions/6_reporting.py
+++ solutions/6_reporting.py
@@ -1,8 +1,8 @@
 import datetime as dt
 
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.sensors.external_task import ExternalTaskSensor
 
 """
 Exercise 6
@@ -20,7 +20,7 @@ processing_dag = DAG(
     default_args={"owner": "Processing Team"},
     schedule="@daily",
     start_date=dt.datetime(2025, 1, 1),
-    end_date=dt.datetime(2025, 1, 15),
+    end_date=dt.datetime(2026, 3, 1),
 )
 
 reporting_dag = DAG(
@@ -29,7 +29,7 @@ reporting_dag = DAG(
     default_args={"owner": "Reporting Team"},
     schedule="0 6 * * *",
     start_date=dt.datetime(2025, 1, 1),
-    end_date=dt.datetime(2025, 1, 15),
+    end_date=dt.datetime(2026, 3, 1),
 )
 
 with processing_dag:
@@ -42,6 +42,7 @@ with reporting_dag:
         task_id="processing_done",
         external_dag_id="solution_6_processing_pipeline",
         external_task_id="done",
+        deferrable=True,
         execution_delta=dt.timedelta(hours=6),
     )
 
